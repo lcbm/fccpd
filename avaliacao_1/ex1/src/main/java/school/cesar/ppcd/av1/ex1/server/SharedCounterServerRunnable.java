62d0ba9bbcb5db496f390bab465f79a514f943fd
Leonardo Melo <lcbm@cesar.school>
2020-04-21 15:14:50 -0300

    feat(avaliacao_1): add resolution for ex1

--- avaliacao_1/ex1/src/main/java/school/cesar/ppcd/av1/ex1/server/SharedCounterServerRunnable.java
+++ avaliacao_1/ex1/src/main/java/school/cesar/ppcd/av1/ex1/server/SharedCounterServerRunnable.java
@@ -1,13 +1,39 @@
 package school.cesar.ppcd.av1.ex1.server;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
 import java.net.Socket;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class SharedCounterServerRunnable implements Runnable {
 	private Socket client;
-	public SharedCounterServerRunnable(Socket client) {
+	private DataOutputStream dataOutputStream;
+	private DataInputStream dataInputStream;
+	private static final byte[] INC = new byte[] { 'I' };
+	private static AtomicInteger counter = new AtomicInteger(0);
+
+	public SharedCounterServerRunnable(Socket client) throws IOException {
 		this.client = client;
+		this.dataInputStream = new DataInputStream(client.getInputStream());
+		this.dataOutputStream = new DataOutputStream(client.getOutputStream());
 	}
+
 	public void run() {
-		//TODO: Auto-generated method stub
+		try {
+			int currentValue;
+			while (true) {
+				if (this.dataInputStream.read(INC) == 1) {
+					currentValue = counter.getAndIncrement();
+					this.dataOutputStream.writeInt(currentValue);
+					TimeUnit.SECONDS.sleep(1);
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
 }
