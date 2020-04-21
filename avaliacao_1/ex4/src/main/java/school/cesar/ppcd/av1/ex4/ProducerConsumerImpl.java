2f625314eedc5b75b814021c809013e2ae5fe405
Leonardo Melo <lcbm@cesar.school>
2020-04-21 18:18:05 -0300

    feat(avaliacao_1): add resolution for ex4

--- avaliacao_1/ex4/src/main/java/school/cesar/ppcd/av1/ex4/ProducerConsumerImpl.java
+++ avaliacao_1/ex4/src/main/java/school/cesar/ppcd/av1/ex4/ProducerConsumerImpl.java
@@ -1,14 +1,39 @@
 package school.cesar.ppcd.av1.ex4;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class ProducerConsumerImpl implements ProducerConsumer {
+	private static Queue<SomeRequest> queue = new LinkedList<SomeRequest>();;
+	private static final int maxSize = 2;
 
 	public void produce(SomeRequest request) {
-		// TODO Auto-generated method stub
-		
+		synchronized (queue) {
+			try {
+				while (queue.size() == maxSize) {
+					queue.wait();
+				}
+				queue.add(request);
+				queue.notify();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 	public SomeRequest consume() {
-		// TODO Auto-generated method stub
+		try {
+			synchronized (queue) {
+				while (queue.isEmpty()) {
+					queue.wait();
+				}
+				SomeRequest consumed = queue.remove();
+				queue.notify();
+				return consumed;
+			}
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 		return null;
 	}
 
